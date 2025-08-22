5896f6f7af2e91fe132dbca689376d4510260668
alexwen.creates@gmail.com <alexwen.creates@gmail.com>
2025-08-21 20:48:27 -0500

    Add dist.

---
+++ dist/admin/src/index.d.ts
@@ -0,0 +1,8 @@
+declare const _default: {
+    register(app: any): void;
+    bootstrap(app: any): void;
+    registerTrads({ locales }: {
+        locales: any;
+    }): Promise<any[]>;
+};
+export default _default;
