54fd5cc7634be6165d45fd110e3b7fc88846c7ce
alexwen.creates@gmail.com <alexwen.creates@gmail.com>
2025-08-21 21:53:54 -0500

    update file obj.

--- server/src/services/import/utils/file.js
+++ server/src/services/import/utils/file.js
@@ -76,10 +76,11 @@ const importFile = async ({ url, name, alternativeText, caption }, user) => {
       .upload(
         {
           files: {
-            name: file.name,
-            type: file.type,
+            originalFilename: file.name,
+            mimetype: file.type,
             size: file.size,
             filepath: file.path,
+
           },
           data: {
             fileInfo: {
@@ -112,10 +113,10 @@ const fetchFile = async (url) => {
     const fileData = getFileDataFromRawUrl(url);
     const filePath = await writeFile(fileData.name, buffer);
     return {
-      name: fileData.name,
-      type: contentType,
+      originalFilename: fileData.name,
+      mimetype: contentType,
       size: contentLength,
-      path: filePath,
+      filepath: filePath,
     };
   } catch (error) {
     throw new Error(`Tried to fetch file from url ${url} but failed with error: ${error.message}`);
